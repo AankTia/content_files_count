959a31a2bcaf7d33cad2aeb732ebdf7225d34d54
Tia Widi <tia_aank@yahoo.co.id>
2018-04-17 13:54:58 +0700

    Engine - Implement read_content function

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,5 +1,9 @@
-class Lib
-  module Engine
-    
+module Engine
+  def self.read_content(path)
+    results = []
+    Dir.glob(path).each do |file_name|      
+      results << IO.read(file_name) unless File.directory? file_name
+    end
+    results
   end
 end
