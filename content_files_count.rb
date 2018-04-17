91de1ab522f41f9eecb24db273191a2b113704e1
Tia Widi <tia_aank@yahoo.co.id>
2018-04-17 14:54:47 +0700

    Implement Main Class

--- content_files_count.rb
+++ content_files_count.rb
@@ -1,5 +1,25 @@
 require './lib/engine.rb'
 
-class ContentFilesCount
-  
+class ContentFilesCount  
+  if ARGV.length != 1
+    puts 'We need exactly one path directory parameter'
+    path = nil
+  else
+    path = (ARGV[0] != nil) ? "#{ARGV[0]}/**/*" : nil
+  end
+
+  if path != nil
+    file_contents = Engine.read_content(path)
+    result = Engine.count_contents(file_contents)
+
+    puts 'Number of File with Same Contents'
+    result.contents_count.each { |content,value| puts "#{content}: #{value}" }
+    puts 
+
+    puts 'Max Similar Content'
+    result.max_similar_content.each { |content,value| puts "#{content}: #{value}"}
+  else
+    puts 'Missing Path Directory. Please specify target Directory'
+    puts 'Eq: ruby content_files_count.rb ./document'
+  end
 end
